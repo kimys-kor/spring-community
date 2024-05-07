bdd3c216f7bb528b839e56861b97d31860640f30
kimys <ys2life1@naver.com>
2024-05-08 00:19:30 +0900

    게시글작성 valid조건 수정

--- api/src/main/java/com/community/api/model/dto/SavePostDto.java
+++ api/src/main/java/com/community/api/model/dto/SavePostDto.java
@@ -1,13 +1,14 @@
 package com.community.api.model.dto;
 
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import lombok.Builder;
 
 @Builder
 public record SavePostDto(
-        @NotBlank(message = "postType must not be blank")
+        @NotNull(message = "postType must not be blank")
         int postType,
-        @NotBlank(message = "notification must not be blank")
+        @NotNull(message = "notification must not be blank")
         boolean notification,
         @NotBlank(message = "title must not be blank")
         String title,

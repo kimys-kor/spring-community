84ef184356455f6a408c4a46a5c3f0621ed0fbd0
kimys <ys2life1@naver.com>
2024-05-08 00:21:52 +0900

    댓글 valid 조건 수정

--- api/src/main/java/com/community/api/model/dto/SaveCommentDto.java
+++ api/src/main/java/com/community/api/model/dto/SaveCommentDto.java
@@ -1,11 +1,12 @@
 package com.community.api.model.dto;
 
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import lombok.Builder;
 
 @Builder
 public record SaveCommentDto(
-        @NotBlank(message = "boardId must not be blank")
+        @NotNull(message = "boardId must not be blank")
         Long boardId,
         Long parentId,
         @NotBlank(message = "content must not be blank")

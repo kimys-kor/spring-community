0db55e08d7b59fbc0e826d3c37e70981f279a6f7
kimys <ys2life1@naver.com>
2024-05-21 19:32:20 +0900

    로그인 통합,롤에따라 제약조건수정

--- api/src/main/java/com/community/api/common/ipauthentication/GuestPathIntercepter.java
+++ api/src/main/java/com/community/api/common/ipauthentication/GuestPathIntercepter.java
@@ -1,6 +1,11 @@
 package com.community.api.common.ipauthentication;
 
+import com.community.api.common.jwt.JwtTokenProvider;
+import com.community.api.common.properties.JwtProperties;
+import com.community.api.model.User;
+import com.community.api.model.base.UserRole;
 import com.community.api.service.IpService;
+import com.community.api.service.UserService;
 import jakarta.annotation.Resource;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
@@ -11,11 +16,30 @@ public class GuestPathIntercepter implements HandlerInterceptor {
 
     @Resource
     private IpService ipService;
+    @Resource
+    private UserService userService;
+    @Resource
+    private JwtProperties jwtProperties;
+    @Resource
+    private JwtTokenProvider jwtTokenProvider;
 
     @Override
     public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
-        String ipAddress = request.getRemoteAddr();
-        return ipService.findIp("blocked", ipAddress);
+        //FIXME 로그인이 하나로 통합되었기 때문에 role에 따라서 blocked, approved 수정해주어야함
+        String Authorization = request.getHeader(jwtProperties.headerString());
+        String token = Authorization.replace("Bearer ", "");
+        String username = jwtTokenProvider.resolveToken(token);
+        User user = userService.findByUsername(username);
+
+        if (user.getRole().equals(UserRole.ROLE_USER)) {
+            String ipAddress = request.getRemoteAddr();
+            return ipService.findIp("blocked", ipAddress);
+        } else {
+            String ipAddress = request.getRemoteAddr();
+            return ipService.findIp("approved", ipAddress);
+        }
+
+
     }
 
     @Override
